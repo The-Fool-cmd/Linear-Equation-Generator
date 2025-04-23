c3070346087252d6a9eedc391e87315a4779b9c2
Fool <183977343+The-Fool-cmd@users.noreply.github.com>
2025-04-23 05:27:28 +0300

    Fixing the mess i made

--- setup_linux.sh
+++ setup_linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get update && sudo apt install python3.12-venv
+sudo apt-get update && sudo apt-get install -y python3-tk && sudo apt install python3.12-venv
 python3 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
